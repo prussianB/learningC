4258cc4478742780a0c168fa51c1339fc3c03145
Baran Ç <41894963+prussianB@users.noreply.github.com>
2018-08-01 13:49:57 +0300

    Update Reader.c

--- Day 7/Reader.c
+++ Day 7/Reader.c
@@ -2,42 +2,42 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-FILE * fData;
-int val;
-int val1, val2;
+FILE * fDataR;
+FILE * fDataW;
 
 void initialize(){
-	
-	fData = fopen("monitorData.txt", "r");
+
+	fDataR = fopen("monitorData.txt", "r");
+	fDataW = fopen("
+		writeData.txt", "w");
 	return;
 }
 
 void close(){
-	fclose(fData);
+	fclose(fDataR);
+	fclose(fDataW);
 	return;
 }
 
 int read(){
-	if (feof(fData)) {
-		val = -1;
+	if (feof(fDataR)) {
+		return -1;
 	}
-	
-	//fgets string. Gelen önce ! kontrol et. String compare,   2 islem string to int
-	
-	fgets(val1, 5, fData);
-	if (val1 == '!') {
-		val = -2;
+
+	//fgets string. Gelen �nce ! kontrol et. String compare,   2 islem string to int
+	char val[10];
+	fgets(val, 10, fDataR);
+	if (strcmp(val, "!\n") == 0 || strcmp(val, "!") == 0) {
+		return -2;
 	}
-	return val;
+	return atoi(val);
 }
 
-	//Dosya biterse -1 dön.
-	// ! gelirse -2 dön.
+//Dosya biterse -1 d�n.
+// ! gelirse -2 d�n.
 
-void write(int val1, int val2 ){
-	val2 = val1 * 2;
-	printf("%d;%d\n", val1, val2);
-	return;
+void write(int val1, int val2){
+	fprintf(fDataW, "%d,%d\n", val1, val2);
 
 	// 256;512
 	// 128;256
@@ -45,20 +45,18 @@ void write(int val1, int val2 ){
 }
 
 
-
-
-
 int main(){
+	int val;
 	initialize();
 	while (true) {
-		read();
+		val = read();
 		if (val == -1){
 			break;
 		}
 		if (val == -2){
 			continue;
 		}
-		write(val1, val2);
+		write(val, val*2);
 	}
 	close();
 	return 0;
